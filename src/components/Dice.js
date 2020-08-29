7c91e833a37c5139ae9976dbc6961bd27c17482f
alexander747 <elalexander747@gmail.com>
2020-08-28 22:17:32 -0500

    dice

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
-import "../mystyles.css";
+import "./../mystyles.css";
+import img1 from "./../img/1.png";
+import img2 from "./../img/2.png";
+import img3 from "./../img/3.png";
+import img4 from "./../img/4.png";
+import img5 from "./../img/5.png";
+import img6 from "./../img/6.jpg";
 
 export default function Dice() {
-  const [n, setNumber] = useState(1);
+  const [getImg, setImg] = useState(img1);
+  const number = () => {
+    let number = Math.floor(Math.random() * (7 - 1)) + 1;
+    console.log(number);
+    switch (number) {
+      case 1:
+        setImg(img1);
+        break;
+      case 2:
+        setImg(img2);
+        break;
+      case 3:
+        setImg(img3);
+        break;
+      case 4:
+        setImg(img4);
+        break;
 
-  function randomNumber() {
-    console.log("click");
-    setNumber(Math.round(Math.random() * 5) + 1);
-  }
+      case 5:
+        setImg(img5);
+        break;
 
+      default:
+        setImg(img6);
+        break;
+    }
+  };
   return (
-    <div>
-      <button onClick={randomNumber}>Roll Dice {n}</button>
-      <div className="Dice">hola</div>
+    <div onClick={number}>
+      <img className="styledice" src={getImg}></img>
     </div>
   );
 }
